9bb0e2d27be0198463b2e0574ffe0e2b0f74da22
ejpi1 <roro0010@naver.com>
2023-11-15 19:59:17 +0900

    간단하게 LED를 켬

---
+++ lighting/lighting.ino
@@ -0,0 +1,12 @@
+#define LEDPIN  8
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+  digitalwrite(LEDPIN, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

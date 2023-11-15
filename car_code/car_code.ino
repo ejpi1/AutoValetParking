326a89024576ccbc506297d5daaf495a0cec58e7
ejpi1 <roro0010@naver.com>
2023-11-15 19:28:01 +0900

    가독성 향상을 위한 indent 수정, 변수 선언을 void loop() 밖으로 꺼냄

--- car_code/car_code.ino
+++ car_code/car_code.ino
@@ -4,7 +4,10 @@ int IN3Pin = 11;
 int IN4Pin = 10;
 int ENAPin = A0;
 int ENBPin = A1;
-void setup() {
+
+char in_data;             // 입력된 데이터 저장을 위한 변수
+
+void setup(){
   Serial.begin(9600);
   pinMode(IN1Pin, OUTPUT);
   pinMode(IN2Pin, OUTPUT);
@@ -13,50 +16,38 @@ void setup() {
   analogWrite(ENAPin, 255);
   analogWrite(ENBPin, 255);
 }
-void loop()
-{
-   if(Serial.available())  //시리얼모니터에서 데이터가 들어오면
-  {
-    char in_data;         // 입력된 데이터 저장을 위한 변수
+void loop(){
+  if(Serial.available()){  //시리얼모니터에서 데이터가 들어오면
     in_data = Serial.read();  //입력된 데이터 in_data에 저장
-     Serial.print("data : ");
-     Serial.println(in_data);
-     if(in_data == 's')
-     {
-     digitalWrite(IN1Pin, HIGH);
-     digitalWrite(IN2Pin, LOW);
-     digitalWrite(IN3Pin, HIGH);
-     digitalWrite(IN4Pin, LOW);
-     delay(1000);
-     }
-     else if(in_data == 'w')
-     {
-     digitalWrite(IN1Pin, LOW);
-     digitalWrite(IN2Pin, HIGH);
-     digitalWrite(IN3Pin, LOW);
-     digitalWrite(IN4Pin, HIGH);
-     delay(1000);
-     }
-     else if(in_data == 'd')
-     {
-     digitalWrite(IN1Pin, HIGH);
-     digitalWrite(IN2Pin, LOW);
-     digitalWrite(IN3Pin, LOW);
-     digitalWrite(IN4Pin, HIGH);
-     }
-     else if(in_data == 'a')
-     {
-     digitalWrite(IN1Pin, LOW);
-     digitalWrite(IN2Pin, HIGH);
-     digitalWrite(IN3Pin, HIGH);
-     digitalWrite(IN4Pin, LOW);
-     }
-     else if(in_data == 'x')
-     {
-     digitalWrite(IN1Pin, HIGH);
-     digitalWrite(IN2Pin, HIGH);
-     digitalWrite(IN3Pin, HIGH);
-     digitalWrite(IN4Pin, HIGH);
-     }
+    Serial.println(in_data);
+    Serial.print("data : ");
+      if(in_data == 's'){
+        digitalWrite(IN1Pin, HIGH);
+        digitalWrite(IN2Pin, LOW);
+        digitalWrite(IN3Pin, HIGH);
+        digitalWrite(IN4Pin, LOW);
+        delay(1000);
+      }else if(in_data == 'w'){
+        digitalWrite(IN1Pin, LOW);
+        digitalWrite(IN2Pin, HIGH);
+        digitalWrite(IN3Pin, LOW);
+        digitalWrite(IN4Pin, HIGH);
+        delay(1000);
+      }else if(in_data == 'd'){
+        digitalWrite(IN1Pin, HIGH);
+        digitalWrite(IN2Pin, LOW);
+        digitalWrite(IN3Pin, LOW);
+        digitalWrite(IN4Pin, HIGH);
+      }else if(in_data == 'a'){
+        digitalWrite(IN1Pin, LOW);
+        digitalWrite(IN2Pin, HIGH);
+        digitalWrite(IN3Pin, HIGH);
+        digitalWrite(IN4Pin, LOW);
+      }else if(in_data == 'x'){
+        digitalWrite(IN1Pin, HIGH);
+        digitalWrite(IN2Pin, HIGH);
+        digitalWrite(IN3Pin, HIGH);
+        digitalWrite(IN4Pin, HIGH);
+      }
   }
 }

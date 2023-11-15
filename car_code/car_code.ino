ed00abc4eb9f939065c84e21923a246cfce66d05
ejpi1 <roro0010@naver.com>
2023-11-15 19:32:20 +0900

    가동성 향상을 위한 indent 재수정

--- car_code/car_code.ino
+++ car_code/car_code.ino
@@ -5,7 +5,7 @@ int IN4Pin = 10;
 int ENAPin = A0;
 int ENBPin = A1;
 
-char in_data;             // 입력된 데이터 저장을 위한 변수
+char in_data;					//입력된 데이터 저장을 위한 변수
 
 void setup(){
   Serial.begin(9600);
@@ -16,38 +16,39 @@ void setup(){
   analogWrite(ENAPin, 255);
   analogWrite(ENBPin, 255);
 }
+
 void loop(){
-  if(Serial.available()){  //시리얼모니터에서 데이터가 들어오면
-    in_data = Serial.read();  //입력된 데이터 in_data에 저장
+  if(Serial.available()){		//시리얼모니터에서 데이터가 들어오면
+    in_data = Serial.read();	//입력된 데이터 in_data에 저장
     Serial.println(in_data);
     Serial.print("data : ");
-      if(in_data == 's'){
-        digitalWrite(IN1Pin, HIGH);
-        digitalWrite(IN2Pin, LOW);
-        digitalWrite(IN3Pin, HIGH);
-        digitalWrite(IN4Pin, LOW);
-        delay(1000);
-      }else if(in_data == 'w'){
-        digitalWrite(IN1Pin, LOW);
-        digitalWrite(IN2Pin, HIGH);
-        digitalWrite(IN3Pin, LOW);
-        digitalWrite(IN4Pin, HIGH);
-        delay(1000);
-      }else if(in_data == 'd'){
-        digitalWrite(IN1Pin, HIGH);
-        digitalWrite(IN2Pin, LOW);
-        digitalWrite(IN3Pin, LOW);
-        digitalWrite(IN4Pin, HIGH);
-      }else if(in_data == 'a'){
-        digitalWrite(IN1Pin, LOW);
-        digitalWrite(IN2Pin, HIGH);
-        digitalWrite(IN3Pin, HIGH);
-        digitalWrite(IN4Pin, LOW);
-      }else if(in_data == 'x'){
-        digitalWrite(IN1Pin, HIGH);
-        digitalWrite(IN2Pin, HIGH);
-        digitalWrite(IN3Pin, HIGH);
-        digitalWrite(IN4Pin, HIGH);
-      }
+    if(in_data == 's'){			//후진
+      digitalWrite(IN1Pin, HIGH);
+      digitalWrite(IN2Pin, LOW);
+      digitalWrite(IN3Pin, HIGH);
+      digitalWrite(IN4Pin, LOW);
+      delay(1000);
+    }else if(in_data == 'w'){	//전진
+      digitalWrite(IN1Pin, LOW);
+      digitalWrite(IN2Pin, HIGH);
+      digitalWrite(IN3Pin, LOW);
+      digitalWrite(IN4Pin, HIGH);
+      delay(1000);
+    }else if(in_data == 'd'){	//우회전
+      digitalWrite(IN1Pin, HIGH);
+      digitalWrite(IN2Pin, LOW);
+      digitalWrite(IN3Pin, LOW);
+      digitalWrite(IN4Pin, HIGH);
+    }else if(in_data == 'a'){	//좌회전
+      digitalWrite(IN1Pin, LOW);
+      digitalWrite(IN2Pin, HIGH);
+      digitalWrite(IN3Pin, HIGH);
+      digitalWrite(IN4Pin, LOW);
+    }else if(in_data == 'x'){	//정지
+      digitalWrite(IN1Pin, HIGH);
+      digitalWrite(IN2Pin, HIGH);
+      digitalWrite(IN3Pin, HIGH);
+      digitalWrite(IN4Pin, HIGH);
+    }
   }
 }

7e3edd9317d20c13e8dfddcc963a2ce2ee0c476b
ejpi1 <roro0010@naver.com>
2023-11-22 11:57:36 +0900

    LED 깜빡이도록 수정, 아직 하나만

--- lighting/lighting.ino
+++ lighting/lighting.ino
@@ -1,12 +1,12 @@
-#define LEDPIN  8
+#define LEDPIN      8
+#define FREQUENCY   31 //[Hz]
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LEDPIN, OUTPUT);
-  digitalWrite(LEDPIN, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  tone(LEDPIN, FREQUENCY);
+  delay(1000);
 }

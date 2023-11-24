20f26178aec1a2a7f9ec924c1b816b0c5fca02f9
ejpi1 <roro0010@naver.com>
2023-11-24 09:31:07 +0900

    HC-05 하나만 사용으로 변경

--- HC-05_test/HC-05_test.ino
+++ HC-05_test/HC-05_test.ino
@@ -5,47 +5,20 @@
 // 가급적 TX, RX PIN도 제거한 상태에서 VCC선을 뺏다가 꼽을 것
 // LED가 2초마다 점멸하면 AT Command 모드이다.
 
-SoftwareSerial BTSerial1(8, 9); // Arduino RX, TX
-SoftwareSerial BTSerial2(12, 13);
+SoftwareSerial BTSerial(8, 9); // Arduino RX, TX
 // 아두이노의 TX, RX를 각각 HC-05의 RX, TX에 연결할 것
-
-int BTState = 0;                // 기본값은 BTSerial1과 통신
-int serial_data = 0;           // Serial.read()의 값 저장
-int is_command = 0;             // BTSerial 변경하는 명령어라면 1
 
 void setup() {
   Serial.begin(9600);
-  BTSerial1.begin(38400);  // HC-05는 기본적으로 9600 or 38400 baud rate
-  BTSerial2.begin(38400);
+  BTSerial.begin(9600);  // HC-05는 기본적으로 9600 or 38400 baud rate
   Serial.println("AT코드를 입력하세요:");
 }
 
 void loop() {
+  if(BTSerial.available()){
+    Serial.write(BTSerial.read());
+  }
   if(Serial.available()){
-    serial_data = Serial.read();
-    Serial.print(serial_data, HEX);
-    if(serial_data == "49" || serial_data == "50"){
-      BTState = serial_data - 48;
-      is_command = 1;
-      Serial.print("BT");
-      Serial.print(BTState);
-      Serial.println("으로 변경되었습니다.");
-    }
-    if(!is_command){
-      if(BTState == 1){
-        BTSerial1.write(serial_data);
-      }else if(BTState == 2){
-        BTSerial2.write(serial_data);
-      }
-    }
-  }
-
-  if(BTSerial1.available()){
-    Serial.print("From BT1: ");
-    Serial.write(BTSerial1.read());
-  }
-  if(BTSerial2.available()){
-    Serial.print("From BT2: ");
-    Serial.write(BTSerial2.read());
+    BTSerial.write(Serial.read());
   }
 }

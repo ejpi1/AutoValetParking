90ea392811e606ec879e17b9911f0dc3de2100fa
LIM JAE HYUK <blacksketch19>
2023-12-06 12:50:18 +0900

    좌륜 모터 정지

--- car_code/drive.ino
+++ car_code/drive.ino
@@ -12,13 +12,13 @@ void drive(){               // 'a', 'd', 's', 'w'에 따라 차량 움직임
   }else if(car_move == 'd'){
     digitalWrite(IN1Pin, HIGH);
     digitalWrite(IN2Pin, LOW);
-    digitalWrite(IN3Pin, LOW);
+    digitalWrite(IN3Pin, HIGH);
     digitalWrite(IN4Pin, HIGH);
   }else if(car_move == 'a'){
     digitalWrite(IN1Pin, LOW);
     digitalWrite(IN2Pin, HIGH);
     digitalWrite(IN3Pin, HIGH);
-    digitalWrite(IN4Pin, LOW);
+    digitalWrite(IN4Pin, HIGH);
   }else if(car_move == 'x'){
     digitalWrite(IN1Pin, HIGH);
     digitalWrite(IN2Pin, HIGH);

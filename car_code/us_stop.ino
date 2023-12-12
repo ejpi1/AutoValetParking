b768e751f7ebea2fadd0a50552a4a99651826888
ejpi1 <roro0010@naver.com>
2023-12-12 13:17:01 +0900

    초음파 거리 5cm로 수정

--- car_code/us_stop.ino
+++ car_code/us_stop.ino
@@ -1,5 +1,5 @@
 void us_stop(){                 // 초음파 거리가 일정 미만이면 차량 정지
-  if((distance < 15) && (distance > 1)){
+  if((distance < 5) && (distance > 1)){   // 가급적 3cm미만까지 내리지 말 것
     drive('x');
     no_periodic();
   }
